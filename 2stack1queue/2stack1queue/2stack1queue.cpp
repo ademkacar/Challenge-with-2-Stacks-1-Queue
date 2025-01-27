cb6a728745681e2de91b24e3a8128b6b2289d846
Adem Kaçar <166117926+ademkacar@users.noreply.github.com>
2025-01-27 21:44:36 +0300

    Add files via upload

---
+++ 2stack1queue/2stack1queue/2stack1queue.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+#include <stack>
+using namespace std;
+
+void print(stack <int> a) {
+    
+    while (!a.empty()) {
+        cout << a.top() << " ";
+        a.pop();
+    }
+    cout << endl;
+}
+
+
+int main()
+{
+    stack <int> s1;
+    stack <int> s2;
+
+    s1.push(10);
+    s1.push(20);
+    s1.push(30);
+
+    print(s1);
+
+    while (!s1.empty()) {
+        s2.push(s1.top());
+        s1.pop();
+    }
+
+
+    print(s2);
+
+
+    return 0;
+}
